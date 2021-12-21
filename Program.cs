1fdcce691761ce22feefad673daa99b423df5085
usk1129 <k_usko@yahoo.com>
2021-12-21 11:52:37 +0100

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Garage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            AirPlane airplay = new AirPlane(1129, "yellow", 11, 11);
+            //garage<int> Garage_test = new garage<int>(50);
+            ////setting values
+            //for (int c = 0; c < 5; c++)
+            //{
+            //    Garage_test.setItem(c, c * 5);
+            //}
+
+            //for (int c = 0; c < 5; c++)
+            //{
+            //    Console.Write(Garage_test.getItem(c) + " ");
+            //}
+
+            Console.WriteLine();
+            var s = Garage_test.GetVehicles();
+            Console.WriteLine(s);
+
+
         }
     }
 }
