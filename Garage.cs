76091141877e853869476ec5d4ae8e0c653ddf91
usk1129 <k_usko@yahoo.com>
2021-12-21 11:52:34 +0100

    Garage class generic

---
+++ Garage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class Garage<T> where T : Vehicle, IEnumerable
+    {
+        protected T[] Vehicles;
+        private int capacity;
+
+        public Garage( int capacity)
+        {
+            this.capacity = Math.Max(capacity, 1);
+            Vehicles = new T[capacity];
+
+        }
+
+        public T getVehiclebyIndex(int index)
+        {
+            return Vehicles[index];
+        }
+
+        public IEnumerator GetVehicles()
+        {
+            foreach(var v in Vehicles)
+            {
+                yield return v;
+            }
+        }
+
+    }
+}
